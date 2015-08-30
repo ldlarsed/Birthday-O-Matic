eb4f42ac08135c550abb7640748c1381e7d30225
Lukas Larsed <git@lukaslarsed.com>
2015-08-30 13:32:41 +0200

    Added a user name prompt

--- Obliger/Mappe1/Hangman/app/src/main/java/com/example/s198569/hangman/StartGame.java
+++ Obliger/Mappe1/Hangman/app/src/main/java/com/example/s198569/hangman/StartGame.java
@@ -1,17 +1,29 @@
 package com.example.s198569.hangman;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 
 public class StartGame extends ActionBarActivity {
+
+    private ImageButton button_startGame;
+    private EditText userName;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +35,44 @@ public class StartGame extends ActionBarActivity {
         Animation titleAnimation = AnimationUtils.loadAnimation(this, R.anim.title_anim);
         ImageView hangman_title__image = (ImageView) findViewById(R.id.hangman_title_image);
         Animation hangmanTitleAnimation = AnimationUtils.loadAnimation(this, R.anim.hangman_anim);
-
         title_image.startAnimation(titleAnimation);
         hangman_title__image.startAnimation(hangmanTitleAnimation);
+
+        /* The sign in dialog that allows user to enter it's name. */
+        //Fetching the start game button
+        button_startGame = (ImageButton) findViewById(R.id.btn_startGame);
+        userName = (EditText) findViewById(R.id.user_Name);
+
+        button_startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+                View promtView = layoutInflater.inflate(R.layout.dialog_user_name, null);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setView(promtView);
+
+                final EditText input = (EditText) promtView.findViewById(R.id.user_Name);
+
+                //Set up for the user name dialog window
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Start", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog dialog_userInputName = alertDialogBuilder.create();
+                dialog_userInputName.show();
+            }
+        });
     }
 
     @Override
@@ -50,6 +97,10 @@ public class StartGame extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Configures the immersive state of an application where both navigation and status bar are hidden and require user to swipe from upper or lower side of screen to activate.
+     * @param hasFocus
+     */
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -66,19 +117,22 @@ public class StartGame extends ActionBarActivity {
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 
-/*    private void hideSystemUI() {
-        // Set the IMMERSIVE flag.
-        // Set the content to appear under the system bars so that the content
-        // doesn't resize when the system bars hide and show.
-        View mDecorView = getWindow().getDecorView();
 
-        mDecorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-    }*/
+    /*
+    Following are the methods to start screen of application.
+     */
+
+    /**
+     * Since the OS on android handles the application exit this will basically put application to standby and exit the home screen.
+     * @param v
+     */
+    public void quitApp(View v){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
 
 }
