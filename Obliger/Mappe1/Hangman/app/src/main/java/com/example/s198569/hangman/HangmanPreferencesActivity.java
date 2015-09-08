7e4be6948f1cf7666e0a1c33cc9c21f3c299b8ed
Lukas Larsed <git@lukaslarsed.com>
2015-09-08 14:08:00 +0200

    Changing of application language works

--- Obliger/Mappe1/Hangman/app/src/main/java/com/example/s198569/hangman/HangmanPreferencesActivity.java
+++ Obliger/Mappe1/Hangman/app/src/main/java/com/example/s198569/hangman/HangmanPreferencesActivity.java
@@ -1,12 +1,19 @@
 package com.example.s198569.hangman;
 
+import android.app.Application;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 
 public class HangmanPreferencesActivity extends PreferenceActivity {
@@ -33,6 +40,16 @@ public class HangmanPreferencesActivity extends PreferenceActivity {
                     String j = prefs.getString("languageChooser", "1");
                     Log.w("HANGMAN", "Language settings choosen: " + j);
 
+                    String locale_language;
+                    if(j.equals("1"))locale_language = "en_US";
+                    else locale_language="nb_NO";
+
+                    Locale locale = new Locale(locale_language);
+                    Locale.setDefault(locale);
+                    Configuration config = new Configuration();
+                    config.locale = locale;
+                    getApplicationContext().getResources().updateConfiguration(config, null);
+                    startActivity(new Intent(HangmanPreferencesActivity.this, StartGameActivity.class));
                 }
             }
         };
