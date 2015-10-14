d0735d427bf2858703cd04ded5a86618866e4c2a
Lukas Larsed <git@lukaslarsed.com>
2015-10-14 23:15:02 +0200

    RegisterPerson completed

--- Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/RegisterPerson.java
+++ Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/RegisterPerson.java
@@ -3,16 +3,33 @@ package com.example.s198569_mappe2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.example.s198569_mappe2.BOL.Person;
+import com.example.s198569_mappe2.LIB.Constants;
+
+import java.util.Date;
 
 public class RegisterPerson extends AppCompatActivity {
+
+    private EditText nameText, phoneText;
+    private DatePicker bDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_person);
+
+        nameText = (EditText) findViewById(R.id.addnewNameEdit);
+        phoneText = (EditText) findViewById(R.id.addnewPhoneEdit);
+        bDate = (DatePicker) findViewById(R.id.addnewDatePicker);
+
     }
 
     @Override
@@ -37,9 +54,46 @@ public class RegisterPerson extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    private boolean isValid(){
+        boolean nameOK = false;
+        boolean phoneOK = false;
+        if(!nameText.getText().toString().matches(Constants.REGULAR_EXPRESSION_LETTERS_ONLY)){
+            nameText.requestFocus();
+            nameText.setError(getString(R.string.regex_letters_only));
+        }else
+            nameOK = true;
+        if(!phoneText.getText().toString().matches(Constants.REGULAR_EXPRESSION_NUMBERS_ONLY)){
+            phoneText.requestFocus();
+            phoneText.setError(getString(R.string.regex_numbers_only));
+        }else
+            phoneOK = true;
+        return (nameOK && phoneOK);
+    }
+
+    private void showLog(){
+        Log.i(Constants.TAG_INPUT, nameText.getText().toString());
+        Log.i(Constants.TAG_INPUT, phoneText.getText().toString());
+        String dateInput = bDate.getDayOfMonth() + "." + bDate.getMonth() + "." + bDate.getYear();
+        Log.i(Constants.TAG_INPUT, dateInput);
+    }
+
+    private Person getInputData(){
+        String name = nameText.getText().toString();
+        String pNumber = phoneText.getText().toString();
+        long unixDate = bDate.getCalendarView().getDate();
+        Date birthdayDate = new Date(unixDate);
+
+        return new Person(name, pNumber, birthdayDate);
+    }
+
     public void addMessage(View view){
-        Intent addNewMessage = new Intent(RegisterPerson.this, RegisterMessage.class);
-        addNewMessage.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        RegisterPerson.this.startActivity(addNewMessage);
+        if(isValid()) {
+            showLog();
+            Intent addNewMessage = new Intent(RegisterPerson.this, RegisterMessage.class);
+            addNewMessage.putExtra(Constants.TAG_PERSON, getInputData());
+            addNewMessage.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            RegisterPerson.this.startActivity(addNewMessage);
+        }
     }
 }
