c84a4dcfa631f0bc69313b38d0e748aee47adbe2
Lukas Larsed <git@lukaslarsed.com>
2015-10-15 16:45:35 +0200

    Visning fra database til listview

---
+++ Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/BuddyList.java
@@ -0,0 +1,13 @@
+package com.example.s198569_mappe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuddyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buddy_list);
+    }
+}
---
+++ Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/BuddyListFragment.java
@@ -0,0 +1,77 @@
+package com.example.s198569_mappe2;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.s198569_mappe2.BOL.Person;
+import com.example.s198569_mappe2.DAL.DBHandler;
+import com.example.s198569_mappe2.LIB.Constants;
+import com.example.s198569_mappe2.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luke on 10/15/15.
+ */
+public class BuddyListFragment extends Fragment {
+
+    private static ArrayAdapter<String> bdAdapter;
+    private static BuddylistEvent buddyListener;
+    private DBHandler db;
+
+    public interface BuddylistEvent{
+        void buddyChanged(Person p);
+    }
+
+
+    public BuddyListFragment() {
+    }
+
+  /*  public void onAttach(Activity activity){
+        super.onAttach(activity);
+
+        try{
+            buddyListener = (BuddylistEvent) activity;
+        }catch (ClassCastException e){
+            Log.e(Constants.TAG_EXCEPTION, Constants.TAG_EXCEPTION_IN + activity.toString() + ": "+e);
+            throw new ClassCastException();
+        }
+    }*/
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.buddy_list_layout, container, false);
+        ListView lv = (ListView) v.findViewById(R.id.buddylistBuddies);
+
+        db = new DBHandler(getActivity());
+
+        final ArrayList<Person> bList = db.getAllBuddies();
+        final ArrayList<String> buddyDetails = new ArrayList<String>();
+
+        buddyDetails.add(bList.get(0).getName());
+
+        /*for (Person s : bList)
+            buddyDetails.add(s.getName());*/
+
+        final ArrayAdapter <String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(),
+                android.R.layout.simple_list_item_1, buddyDetails);
+
+        lv.setAdapter(adapter);
+
+        //Husk å sette en listener for hver item her.
+        //db.close();
+
+        return v;
+    }
+}
