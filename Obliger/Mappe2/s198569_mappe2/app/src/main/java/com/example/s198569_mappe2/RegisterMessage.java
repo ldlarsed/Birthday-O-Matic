cf97c2935b5ed08c05cbac9ceb0807fc205d33c5
Lukas Larsed <git@lukaslarsed.com>
2015-10-19 13:29:24 +0200

    Menubar fixed for the Message activity

--- Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/RegisterMessage.java
+++ Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/RegisterMessage.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.example.s198569_mappe2.BOL.Person;
@@ -20,6 +23,7 @@ import java.util.Date;
 
 public class RegisterMessage extends AppCompatActivity implements DialogYesNoListener {
 
+    private TextView buddyName, buddyBDay;
     private EditText messageText;
     //private TimePicker messageTime;
     private Switch isActiveSwitch;
@@ -29,6 +33,14 @@ public class RegisterMessage extends AppCompatActivity implements DialogYesNoLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar(); //v7 action bar prevents nullpointer
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_register_message);
 
 
@@ -39,10 +51,38 @@ public class RegisterMessage extends AppCompatActivity implements DialogYesNoLis
 
         Intent intent = getIntent();
         person = (Person) intent.getSerializableExtra(Constants.TAG_PERSON);
+
+        //Sets up name and birthdaydate of the newly added buddy i this activity header
+        buddyName = (TextView) findViewById(R.id.addmessageHeaderText);
+        buddyBDay = (TextView) findViewById(R.id.addmessageBirthdayDateText);
+        buddyName.setText(person.getName());
+        buddyBDay.setText(person.getSimpleBirthdayDate());
+
         Log.i(Constants.TAG_PERSON, person.toString());
 
         db = new DBHandler(this);
         //db.addBuddy(person);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_register_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private boolean isValid(){
