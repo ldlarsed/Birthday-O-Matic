a34e6e281eac0087a0b18aa1dbab7d37efc73b64
Lukas Larsed <git@lukaslarsed.com>
2015-10-20 20:28:11 +0200

    Content provider test project complited

--- Obliger/Mappe2/BDayBuddiesContentProviderTest/app/src/main/java/com/example/s198569/bdaybuddiescontentprovidertest/MainActivity.java
+++ Obliger/Mappe2/BDayBuddiesContentProviderTest/app/src/main/java/com/example/s198569/bdaybuddiescontentprovidertest/MainActivity.java
@@ -5,6 +5,11 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,15 +17,35 @@ public class MainActivity extends AppCompatActivity {
     static final String PROVIDER_NAME = "com.example.s198569_mappe2.BDayOMatic";
     static final Uri URL = Uri.parse("content://" + PROVIDER_NAME + "/BDayBuddies");
 
+    private ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = (ListView) findViewById(R.id.listView);
+        List<String> buddies = new ArrayList<String>();
+
+
         Cursor c = getContentResolver().query(URL, null, null, null, null, null);
         //c.moveToFirst();
         while (c.moveToNext()){
             Log.i("Test", c.getString(1) + " " + c.getString(2));
+            buddies.add(c.getInt(0) + " " + c.getString(1) + " " + c.getString(2));
         }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                buddies
+        );
+
+        lv.setAdapter(arrayAdapter);
+
+
+
+
+
     }
 }
