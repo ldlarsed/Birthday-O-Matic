3bf55daa4899c329549689b66eae938f4319ff2d
Lukas Larsed <git@lukaslarsed.com>
2015-10-21 19:58:40 +0200

    Callback from delete buddy

--- Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/BLL/BuddyListAdapter.java
+++ Obliger/Mappe2/s198569_mappe2/app/src/main/java/com/example/s198569_mappe2/BLL/BuddyListAdapter.java
@@ -1,9 +1,14 @@
 package com.example.s198569_mappe2.BLL;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +22,12 @@ import android.widget.TextView;
 import com.example.s198569_mappe2.BOL.Person;
 import com.example.s198569_mappe2.DAL.DBHandler;
 import com.example.s198569_mappe2.LIB.Constants;
+import com.example.s198569_mappe2.LIB.DataSetChangedListener;
+import com.example.s198569_mappe2.LIB.DialogYesNoListener;
 import com.example.s198569_mappe2.R;
 import com.example.s198569_mappe2.RegisterMessage;
 import com.example.s198569_mappe2.RegisterPerson;
+import com.example.s198569_mappe2.fragments.InfoDialogFragment;
 
 import org.w3c.dom.Text;
 
@@ -33,9 +41,10 @@ import static android.support.v4.app.ActivityCompat.startActivityForResult;
 public class BuddyListAdapter extends ArrayAdapter<Person> {
 
     Context context;
-    int resource;
+    int resource, buddyId;
     ArrayList<Person> buddies = null;
     private DBHandler db;
+    private DataSetChangedListener dataSetListener;
 
 
     public BuddyListAdapter(Context context, int resource, ArrayList<Person> objects) {
@@ -43,6 +52,10 @@ public class BuddyListAdapter extends ArrayAdapter<Person> {
         this.context = context;
         this.resource = resource;
         this.buddies = objects;
+    }
+
+    public void setDataChangedSetListener(DataSetChangedListener listener){
+        this.dataSetListener = listener;
     }
 
     @Override
@@ -77,8 +90,14 @@ public class BuddyListAdapter extends ArrayAdapter<Person> {
             @Override
             public void onClick(View view) {
                 Log.i("Listener", "Delete clicked");
-                db = new DBHandler(getContext());
-                db.deleteBuddy(buddy.get_ID());
+                buddyId = buddy.get_ID();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+
+                builder.setMessage(R.string.info_dialog_title_delete_buddy).setPositiveButton(R.string.answer_yes, dialogClickListener)
+                        .setNegativeButton(R.string.answer_no, dialogClickListener).show();
+
             }
         });
 
@@ -105,6 +124,27 @@ public class BuddyListAdapter extends ArrayAdapter<Person> {
 
         return row;
     }
+
+
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+
+                    db = new DBHandler(getContext());
+                    db.deleteBuddy(buddyId);
+                    if(dataSetListener != null){
+                        dataSetListener.dataSetChanged(true);
+                    }
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    //No button clicked
+                    break;
+            }
+        }
+    };
 
     static class BuddyHolder{
         TextView txtName, bDayDate;
